e7ea73e72b9acd7fba680b426ab93b1859b55ff5
nuri-tix <artweb1509@gmail.com>
2022-03-06 23:53:18 +0300

    Add Files18,19

--- src/index.js
+++ src/index.js
@@ -21,7 +21,22 @@ var swiper = new Swiper(".offerSwiper", {
      nextEl: ".swiper-button-next",
      prevEl: ".swiper-button-prev",
    },
-   modules: [Navigation, Pagination],
+  modules: [Navigation, Pagination],
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2
+    },
+    1000: {
+      slidesPerView: 3,
+      slidesPerGroup: 3
+    },
+  }
 });
  
 var swiper = new Swiper(".trendsSwiper", {
@@ -39,4 +54,19 @@ var swiper = new Swiper(".trendsSwiper", {
     prevEl: ".swiper-button-prev",
   },
   modules: [Navigation, Pagination],
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2
+    },
+    1000: {
+      slidesPerView: 3,
+      slidesPerGroup: 3
+    },
+  }
 });
